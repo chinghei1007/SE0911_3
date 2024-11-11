fa6e83825a1d50aa86915838d8f45c706df54d19
ching <chinghei1007@gmail.com>
2024-11-11 21:58:06 +0800

    1111 2158 gameboard: added listDictToCommaDivided, fileExist (used by exportGameboard) gameboard: unfinished importgameboard, expected using checkLineCount, checkFormat, check_Go_and_jai

--- Gameboard.py
+++ Gameboard.py
@@ -1,3 +1,5 @@
+import os.path
+from tkinter import Tk, filedialog
 import instructions as inst
 class Gameboard:
     def __init__(self):
@@ -47,3 +49,80 @@ class Gameboard:
 
                 if rent > 0:
                     print(f"{i+1}. {name}")
+
+    def importGameboard(self,path):
+        pass
+
+    """def exportGameboard(self, listDict):
+        while True:
+            file_path = input("Please enter the file path: ")
+            if self.fileExist(file_path):
+                print(f"File {file_path} already exists.")
+                new_path = input("Please enter a new file name or path.")
+                if self.fileExist(new_path):
+                    print(f"{new_path} already exists, Please try again.")
+                    continue
+                if not inst.double_confirm():
+                    print("Operation cancelled.")
+                    return
+                file_path = new_path
+                file_path = os.path.abspath(file_path)
+            break
+        text = self.listDictToCommaDivided(listDict)
+        with open(file_path, 'w') as file:
+            file.write(text)
+        print(f"Text successfully exported to {file_path}") """
+
+    def exportGameboard(self, listDict):
+        root = Tk()
+        root.withdraw()
+        file_path = filedialog.asksaveasfilename(title="Select Location", defaultextension= ".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+        root.destroy()
+
+        if not file_path:
+            print("No file selected. Operation cancelled.")
+            return
+
+        writeText = self.listDictToCommaDivided(listDict)
+        with open(file_path, 'w') as f:
+            f.write(writeText)
+
+        print(f"Successfully written to {file_path}")
+
+        file_path = os.path.abspath(file_path)
+
+    def listDictToCommaDivided(self, listDict):
+        lines = []
+        for item in listDict:
+            line = f"{item['name']},{item['price']},{item['rent']}"
+            lines.append(line)
+        return "\n".join(lines)
+
+    def fileExist(self,path):
+        return os.path.exists(path)
+
+    def checkLineCount(self,text,linecount):
+        lines = text.strip().split("\n")
+        return len(lines) == linecount
+
+    def checkFormat(self,text):
+        lines = text.strip().split('\n')
+        for line in lines:
+            parts = line.split(',')
+            if len(parts) != 3:
+                print(f"Problem with {line} missing parts, it should be in format of str, int, int")
+                return False
+            elif not (parts[0].isalpha() and parts[1].isdigit() and parts[2].isdigit()):
+                print(f"Problem with {line} format, it should be \"str, int, int\"")
+                return False
+
+        return True
+
+    def check_Go_and_Jail(self,text):
+        lines = text.strip().split('\n')
+        go_count = sum(1 for line in lines if line.startswith('Go'))
+        jail_count = sum(1 for line in lines if line.startswith('GoToJail'))
+        if go_count > 1 or jail_count > 1:
+            print("Please check only ONE go and ONE jail exists")
+            return False
+        return True
